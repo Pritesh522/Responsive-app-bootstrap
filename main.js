aae3bfc7dce6545b429cf3dc394e75e3e7a2d3b1
Pritesh522 <pritesh.patel52@gmail.com>
2018-05-24 09:03:38 -0700

    final commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $("#nxt-one").click(function(){
+        window.location.href='second_page.html';
+     });
+
+     $("#nxt-two").click(function(){
+        window.location.href='third_page.html';
+     });
+
+     $(".img-link").click(function(){
+        window.location.href='forth_page.html';
+     });
+
+     $("#done").click(function(){
+        window.location.href='index.html';
+     });
+});
+
+
+
